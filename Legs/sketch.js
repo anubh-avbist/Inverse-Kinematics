a1286c71b66062d638aaadfb430c36b9793c04bb
Anubhav <bist.anubhav0@gmail.com>
2024-07-26 17:56:57 -0400

    Multiple Edge Detection

--- Legs/sketch.js
+++ Legs/sketch.js
@@ -1,9 +1,27 @@
 const segmentLength = 50;
-const numlegs = 3;
-const fps = 30;
+const numlegs = 5;
+const fps = 60;
+const maxSpeed = 0.5;
+const acceleration = 0.35;
+const wobble = 0.1;
 let floorLevel = 400;
 let creature;
 let time = 0;
+let pressedKeys = {};
+let edges = [];
+
+function setup(){
+	frameRate(fps);
+	createCanvas(500,500);
+	creature = new Body(numlegs); 
+	edges.push(new Edge("linear", [0,floorLevel], [500,floorLevel-50], [250,250]));
+	edges.push(new Edge("linear", [0,floorLevel], [500,floorLevel], [250,250]));
+	edges.push(new Edge("linear", [0,floorLevel], [400,0], [250,250]));
+	edges.push(new Edge("linear", [400,floorLevel], [400,0], [250,250]));
+	//edges.push(new Edge("bezier", [0,0],[500,floorLevel],[[0,200],[250,200]]));
+	
+	
+}
 
 // Math
 
@@ -26,7 +44,6 @@ function addVector2(a,b){
 	return [a[0]+b[0], a[1]+b[1]];
 }
 
-
 function subVector2(a,b){
 	return [a[0]-b[0], a[1]-b[1]];
 }
@@ -35,15 +52,124 @@ function scalarMultiply(scalar, v){
 	return [v[0]*scalar,v[1]*scalar];
 }
 
-function bezierInterpolation(start, end, root, t){
-	 
+function bezierInterpolation(start, end, root, t){ 
+	
 	let x = Math.pow((1-t),2)*start[0] + 2*(1-t)*t*root[0]+t*t*end[0];
 	let y = Math.pow((1-t),2)*start[1] + 2*(1-t)*t*root[1]+t*t*end[1];
 	return[x,y];
 
 }
 
+function Newton(f, fPrime, x, root){
+	
+	return x + (f(x, root)/fPrime(x, root));
+}
 
+
+
+// Classes
+
+class Edge{
+	constructor(type, start, end, nodes){
+		this.type=type;
+		this.start=start;
+		this.end=end;
+		
+		if(type === "bezier"){
+			this.nodes=nodes;
+		}
+	}
+
+	get fun(){
+		let start = this.start;
+		let end = this.end;
+		let nodes = this.nodes;
+		if(this.type === "linear"){
+			return function(t){
+				return [lerp(start[0],end[0],t), lerp(start[1],end[1],t)];
+			};
+		} else if(this.type==="bezier"){
+			return function(t){
+				let output = [];
+				for(let i = 0; i < 2; i++){
+					
+					let firstTerm = scalarMultiply(Math.pow((1-t),3),nodes[i]);
+					let secondTerm = scalarMultiply(t*Math.pow((1-t),2),nodes[i]);
+					let thirdTerm = scalarMultiply((1-t)*Math.pow(t,2),nodes[i]);
+					let fourthTerm = scalarMultiply(Math.pow(t,3),nodes[i]);
+					output.push(addVector2(addVector2(addVector2(firstTerm,secondTerm),thirdTerm),fourthTerm));
+				}
+				return output;
+			};
+		}
+	}
+	
+	get distanceFun(){
+		
+		let start = this.start;
+		let end = this.end;
+		let nodes = this.nodes;
+		if(this.type === "linear"){
+			return function(t,r){
+				let x = r[0];
+				let y = r[1];
+				
+				let firstTerm = Math.pow((start[0]*(1-t) + end[0]*t-x),2);
+				let secondTerm = Math.pow((start[1]*(1-t) + end[1]*t-y),2);
+				return Math.sqrt(firstTerm+secondTerm);
+			};
+		}
+	}
+
+	get distancePrime(){
+		
+		let start = this.start;
+		let end = this.end;
+		let nodes = this.nodes; 
+		let distanceFun = this.distanceFun;
+		if(this.type === "linear"){
+			return function(t,r){ 
+				let firstTerm = (start[0]*(1-t) + end[0]*t-r[0]);
+				let secondTerm = (start[1]*(1-t) + end[1]*t-r[0]);
+				let denominator = distanceFun(t,r);
+				return (firstTerm*(start[0]-end[0]) + secondTerm*(start[1]-end[1]))/denominator;
+			};
+		}
+	}
+
+	drawEdge(){
+		if(this.type === "bezier"){
+			
+			beginShape();
+			noFill();
+			vertex(this.start[0],this.start[1]);
+			bezierVertex(
+				this.nodes[0][0],
+				this.nodes[0][1],
+				this.nodes[1][0],
+				this.nodes[1][1],
+				this.end[0],
+				this.end[1]
+			);
+
+	
+			endShape();
+
+		} else if(this.type === "linear"){
+			stroke(255,0,255);
+			strokeWeight(5);
+			line(this.start[0],this.start[1],this.end[0],this.end[1]);
+
+		}
+	}
+	
+}
+
+class Platform{
+	constructor(){
+		this.edges = [];
+	}
+}
 
 class Leg{
 	constructor(root, dest){
@@ -63,8 +189,9 @@ class Leg{
 		this.orientation = 0;
 		this.transitioning = false;
 		this.stepHeight = 5;
-		this.transTime = 0.4;
+		this.transTime = 0.38;
 		this.startDestination = [0,0];
+		this.tVal = 0.5;
 	}
 
 	debugStuff(){
@@ -82,31 +209,58 @@ class Leg{
 		this.time += 1/fps;	
 		
 		if(this.time < this.transTime){
-			
 			let t = this.time/this.transTime;
-			t = Math.sin(Math.PI/2*t);
-			
-			
-			this.destination = bezierInterpolation(this.startDestination,this.newDestination,this.root,this.time/this.transTime);
+			this.destination = bezierInterpolation(this.startDestination,this.newDestination,this.root,t);
+			// If this destination is away again cuz you were moving, re-pick a new destination.
 
-			// Workable method (ish)
-			/*this.destination[0] = lerp(this.destination[0],this.newDestination[0], this.time/this.transTime);
-			let verticalOrientation = (floorLevel-this.root[1])/Math.abs(this.root[1]-floorLevel);
-			this.destination[1] = this.newDestination[1]-verticalOrientation*Math.sin(this.time/this.transTime*Math.PI)*this.stepHeight;
-			*/
-			// Slerp
-
-			// this.destination = slerp(this.preTransDestination,this.newDestination,this.root,this.time/this.transTime);
-		
 		} else {
 			this.transitioning = false;
 		}
 
 	}
 
+	pickDestination(){ 
+		let possibleTs = [];
+		let possibleDestinations = [];
+		edges.forEach(edge => {
+			if(edge.type === "linear"){
+				let i = 0;
+				let tVal = this.tVal;
+				let foundValue = false;
+				while(i<10){
+
+					if(edge.distanceFun(tVal,this.root) <= this.legSize*2){
+						foundValue = true;
+						break;
+					} else {
+						i++;
+						
+						tVal = Newton(edge.distanceFun, edge.distancePrime, tVal, this.root)+ (Math.random()*2-1)*0.05;
+					}
+				}
+				if(foundValue){ 
+					possibleTs.push(tVal);
+					possibleDestinations.push(edge.fun(tVal));	
+					this.tVal = tVal;
+				} 
+			}
+			
+		});
+		
+		if(possibleDestinations.length>0){
+			
+			let rand = Math.floor(Math.random()*possibleDestinations.length);
+			this.newDestination = possibleDestinations[rand];
+
+		} else {
+			
+		}
+		
+		//this.newDestination[0] = this.root[0]+(this.root[0]-this.destination[0])*(Math.random()*0.75+0.25);
+		//this.newDestination[1] = floorLevel-this.newDestination[0]/500*50;
+	}
 
 	IK(){
-		
 		this.jointPos = [
 			this.root[0] + this.legSize*Math.cos(this.rootAngle), 
 			this.root[1] + this.legSize*Math.sin(this.rootAngle)
@@ -116,7 +270,7 @@ class Leg{
 			this.jointPos[1] + this.legSize*Math.sin(this.jointAngle)
 		];
 
-		
+		 
 		let dist = vector2Distance(this.destination,this.root);
 		
 		this.footPos[0] = lerp(this.footPos[0], this.destination[0], 0.5);
@@ -153,14 +307,14 @@ class Leg{
 			this.footPos = lerp(this.footPos,this.destination,0.5);
 			
 		} else { // If out out range, straigthen out the leg.
-			if(this.anchored || dist > this.legSize*3){ // Transition Anchor
+			if(this.anchored || dist > this.legSize*3 && !this.transitioning){ // Transition Anchor
 
 				// Set Up Transitioning
 				this.transitioning = true;
-				this.stepHeight = Math.random()*15+10;
-				this.startDestination[0] = this.destination[0];
-				this.newDestination[0] = this.root[0]+(this.root[0]-this.destination[0])*Math.random()*0.8;
-				this.newDestination[1] = floorLevel;
+				
+				this.startDestination = this.destination;
+
+				this.pickDestination();
 				this.startDestination = this.destination;
 				this.time = 0;
 			}
@@ -190,48 +344,83 @@ class Body{
 		this.x = 0;
 		this.y = 0;
 		this.legs = [];
+		this.vel = [0,0];
+		this.acc = [0,0]; 
 		for(let i = 0; i < numLegs; i++){
 			this.legs.push(new Leg([this.x,this.y], [Math.random()*500,floorLevel]));
 		}
+	}
+
+	update(deltaTime){
+		this.vel[0] += this.acc[0];
+		this.vel[1] += this.acc[1];
+
+		if(getMagnitude(this.vel)>maxSpeed){
+			let scale = maxSpeed/getMagnitude(this.vel);
+			this.vel[0] *= scale;
+			this.vel[1] *= scale;
+		}
+
+		this.x += this.vel[0]*deltaTime;
+		this.y += this.vel[1]*deltaTime;
 	}
 }
 
 function findEnds(root, angle, length){ 
 	return [root[0]+length*Math.cos(angle), root[1] + length*Math.sin(angle)];
 }
-             
-function setup(){
-	frameRate(fps);
-	createCanvas(500,500);
-	creature = new Body(numlegs); 
-	
-}
 
 function draw(){
 	time+=1/fps;
 	background(51);
-	creature.x=lerp(creature.x,mouseX, 0.5);
-	creature.y=lerp(creature.y,mouseY,0.5);
+	creature.acc = [0,0];
+	if(pressedKeys.a){
+		creature.acc[0] +=-acceleration;
+	}
+	if(pressedKeys.d){ 
+		creature.acc[0] +=acceleration;
+	}
+	if(pressedKeys.w){
+		creature.acc[1] +=-acceleration;
+	}
+	if(pressedKeys.s){
+		creature.acc[1] +=acceleration;
+	}
+	if(!pressedKeys.a && !pressedKeys.s && !pressedKeys.d && !pressedKeys.w){
+		creature.vel[0] = lerp(creature.vel[0],0,0.3);
+		creature.vel[1] = lerp(creature.vel[1],0,0.3);
+	}
+
+
+	//creature.x=lerp(creature.x,mouseX, 0.5);
+	//creature.y=lerp(creature.y,mouseY,0.5);
+	creature.update(deltaTime);
 
 	// Draw floor
+
 	fill(255);
 	noStroke();
-	rect(0,floorLevel,500,500);
+	
 	noFill();
 	
 	strokeWeight(5);
 	stroke(50,200,200);
 
+
+	// Draw edges
+	edges.forEach((edge) => {
+		edge.drawEdge();
+	});
+
 	// Body
+	stroke(50,200,200);
 	ellipse(creature.x,creature.y,50,50);
 
 	// For Each Leg
-
 	for(let i = 0; i < creature.legs.length; i++){
 
 		let leg = creature.legs[i];
 		leg.root = [creature.x,creature.y];
-		
 		leg.IK();
 		//leg.debugStuff();
 		
@@ -240,13 +429,23 @@ function draw(){
 		vertex(creature.x,creature.y);
 		bezierVertex(
 			findEnds(leg.root,leg.rootAngle,leg.legSize)[0],
-			findEnds(leg.root,leg.rootAngle,leg.legSize)[1],
+			findEnds(leg.root,leg.rootAngle,leg.legSize)[1], 
 			findEnds(leg.root,leg.rootAngle,leg.legSize)[0],
 			findEnds(leg.root,leg.rootAngle,leg.legSize)[1],
 			findEnds(findEnds(leg.root,leg.rootAngle,leg.legSize), leg.jointAngle, leg.legSize)[0],
 			findEnds(findEnds(leg.root,leg.rootAngle,leg.legSize), leg.jointAngle, leg.legSize)[1]
 		);
 		endShape();
-
 	}
 }
+
+
+function keyPressed(){
+	pressedKeys[key] = true;
+}
+
+function keyReleased(){
+	delete pressedKeys[key];
+}
+
+
